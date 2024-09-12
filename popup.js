9bd8ed8b3da8ba983947e3fba30ccfcc285af2e8
rafeehcp <rafeeh@blend-ed.com>
2024-09-12 08:52:47 +0530

    Hide element "reader-scrubber" by clicking a button

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-console.log('This is a popup!');
+// Hide an element with name "reader-scrubber" by clicking the button.
+
+document.getElementById('hide-button').addEventListener('click', function () {
+    document.querySelector('reader-scrubber').style.display = 'none';
+});
